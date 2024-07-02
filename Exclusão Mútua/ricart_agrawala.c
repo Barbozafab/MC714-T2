8353fec387d6095436b706cc3c48b4010878be0a
Gabriel Queiroz Rodrigues <55721468+gabrielqr@users.noreply.github.com>
2024-07-01 22:34:16 -0300

    Update ricart_agrawala.c

--- Exclusão Mútua/ricart_agrawala.c
+++ Exclusão Mútua/ricart_agrawala.c
@@ -3,95 +3,105 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
-#include <unistd.h> // Para a função sleep
+#include <unistd.h>  // para usleep
 
-#define MAX_TIMESTAMP 1000
+#define MAX_TIMESTAMP 100
 
-int resource = 0; // recurso - controla quantas vezes a SC foi acessada. Deve ser igual ao tamanho
+int resource = 0;
 
-// Função para verificar se vetor contém pelo menos um valor false
-bool isFalse(bool *v, int size) {
+bool isFalse(bool permissions[], int size) {
     for (int i = 0; i < size; ++i) {
-        if (!v[i]) {
+        if (!permissions[i]) {
             return true;
         }
     }
     return false;
 }
 
-// Função para gerar timestamps aleatórios para cada processo
-void generate_timestamps(int *timestamp, int size) {
-    srand(time(NULL));
-    for (int i = 0; i < size; ++i) {
-        timestamp[i] = rand() % MAX_TIMESTAMP; // Gerando valores aleatórios entre 0 e 999 para timestamps
-    }
-}
-
-// Função para executar o algoritmo Ricart-Agrawala
-void ricart_agrawala(int size, int rank, int *timestamp, bool *permissions) {
+void ricart_agrawala(int size, int rank, int timestamps[], bool permissions[]) {
     MPI_Status status;
-    permissions[rank] = true; // Define sua própria permissão como verdadeira
+    permissions[rank] = true;
 
-    printf("Processo %d está requisitando a SC agora.\n", rank);
-    for (int i = 0; i < size; i++) {
+    printf("Processo %d está solicitando a SE agora.\n", rank);
+    for (int i = 0; i < size; ++i) {
         if (i != rank) {
-            MPI_Send(&timestamp[rank], 1, MPI_INT, i, 0, MPI_COMM_WORLD); // envia seu próprio timestamp para o processo com rank = i
+            MPI_Send(&timestamps[rank], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
     }
-    printf("Processo %d transmitiu uma solicitação com o timestamp %d\n", rank, timestamp[rank]);
+    printf("Processo %d transmitiu uma solicitação com timestamp %d\n", rank, timestamps[rank]);
 
-    MPI_Barrier(MPI_COMM_WORLD); // barreira - mecanismo essencial aqui - prosseguimos apenas quando todas as solicitações foram transmitidas
+    MPI_Barrier(MPI_COMM_WORLD);
 
     int received_timestamp;
-    for (int i = 0; i < size; i++) {
+    int received_count = 0;
+    int received[size - 1][2];
+
+    for (int i = 0; i < size; ++i) {
         if (i != rank) {
             MPI_Recv(&received_timestamp, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
             printf("Processo %d recebeu uma solicitação com timestamp %d do processo %d\n", rank, received_timestamp, i);
+            received[received_count][0] = received_timestamp;
+            received[received_count][1] = i;
+            received_count++;
         }
     }
 
-    MPI_Barrier(MPI_COMM_WORLD); // prosseguimos apenas quando todas as solicitações foram recebidas
+    MPI_Barrier(MPI_COMM_WORLD);
 
-    bool *permissions_received = (bool *)malloc(size * sizeof(bool));
-    for (int i = 0; i < size; i++) {
-        permissions_received[i] = false;
+    int message = 1;
+    int flag;
+
+    for (int i = 0; i < received_count; ++i) {
+        if (timestamps[rank] > received[i][0] || (timestamps[rank] == received[i][0] && rank > received[i][1])) {
+            MPI_Send(&message, 1, MPI_INT, received[i][1], 0, MPI_COMM_WORLD);
+            printf("Processo %d enviando permissão para o processo %d\n", rank, received[i][1]);
+        }
     }
 
-    for (int i = 0; i < size; i++) {
-        if (i != rank) {
-            MPI_Send(&permissions[rank], 1, MPI_C_BOOL, i, 0, MPI_COMM_WORLD); // envia a permissão para todos os outros processos
+    int received_message;
+    while (isFalse(permissions, size)) {
+        MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+        if (flag) {
+            MPI_Recv(&received_message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            permissions[status.MPI_SOURCE] = true;
+            printf("Processo %d recebeu permissão do processo %d\n", rank, status.MPI_SOURCE);
         }
     }
 
     printf("Processo %d está na SC.\n", rank);
     resource++;
-    sleep(1); // simula algum trabalho
-    printf("Processo %d saiu da SC. O recurso foi incrementado.\n", rank);
+    usleep(1500000); // simula algum trabalho
+    printf("Processo %d saiu da SC. Recurso foi incrementado.\n", rank);
 
-    free(permissions_received);
+    for (int i = 0; i < received_count; ++i) {
+        if (timestamps[rank] < received[i][0] || (timestamps[rank] == received[i][0] && rank < received[i][1])) {
+            MPI_Send(&message, 1, MPI_INT, received[i][1], 0, MPI_COMM_WORLD);
+        }
+    }
 }
 
-int main(int argc, char **argv) {
-    printf("Valor do recurso no início: %d\n", resource);
+int main(int argc, char** argv) {
+    srand(time(NULL));
+
     MPI_Init(NULL, NULL);
 
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int *timestamp = (int *)malloc(size * sizeof(int));
-    bool *permissions = (bool *)malloc(size * sizeof(bool));
+    int timestamps[size];
+    bool permissions[size];
 
-    generate_timestamps(timestamp, size); // Gera timestamps aleatórios para cada processo
+    for (int i = 0; i < size; ++i) {
+        timestamps[i] = rand() % MAX_TIMESTAMP; // gera timestamps aleatórios entre 0 e MAX_TIMESTAMP-1
+        permissions[i] = false;
+    }
 
-    ricart_agrawala(size, rank, timestamp, permissions);
+    ricart_agrawala(size, rank, timestamps, permissions);
 
     int total_resource = 0;
     MPI_Allreduce(&resource, &total_resource, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
     printf("Valor do recurso após a execução: %d\n", total_resource);
-
-    free(timestamp);
-    free(permissions);
 
     MPI_Finalize();
 
